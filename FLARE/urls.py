b5b483bc88b073da07ed36a58544614db2bed48b
nokekip <kipnoke@gmail.com>
2024-07-08 18:19:26 +0300

    added community home template

--- FLARE/urls.py
+++ FLARE/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('forecast.urls')),
     path('accounts/', include('users.urls')),
+    path('community/', include('community.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- community/views.py
+++ community/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Community home view
+@login_required
+def community_home(request):
+    return render(request, 'community/index.html')
+
