1231438a345f11c26f3848a3d48af800df94a358
nokekip <kipnoke@gmail.com>
2024-07-09 19:12:57 +0300

    created region schema

--- community/models.py
+++ community/models.py
@@ -1,3 +1,9 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+# Regions model
+class Region(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
