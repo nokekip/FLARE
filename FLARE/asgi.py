8e2cb80c081673a3bf6d62fc011215601c3184a3
nokekip <kipnoke@gmail.com>
2024-07-10 00:53:37 +0300

    configured django to run as ASGI and daphne to take over runserver

--- FLARE/asgi.py
+++ FLARE/asgi.py
@@ -10,7 +10,19 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing  import ProtocolTypeRouter, URLRouter
+import community.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'FLARE.settings')
 
 application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': application,
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            community.routing.websocket_urlpatterns
+        )
+    ),
+})
--- FLARE/settings.py
+++ FLARE/settings.py
@@ -36,6 +36,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'daphne',
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -82,6 +84,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'FLARE.wsgi.application'
+ASGI_APPLICATION = 'FLARE.asgi.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    },
+}
 
 
 # Database
