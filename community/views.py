10ddef2d1947cbb97732ebf268d24843c1cf7809
nokekip <kipnoke@gmail.com>
2024-07-09 19:34:45 +0300

    created form for forum creation

--- community/views.py
+++ community/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .models import Region, Forum
+from .forms import CreateForumForm
 
 # Community home view
 @login_required
 def community_home(request):
+    form = CreateForumForm()
     regions = Region.objects.all()
     forums = Forum.objects.all()
     context = {
         'regions': regions,
-        'forums': forums
+        'forums': forums,
+        'form': form,
     }
 
     return render(request, 'community/index.html', context)
