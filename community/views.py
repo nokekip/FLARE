32fc564e1acb38b95e7c95af48f00a930c6526dd
nokekip <kipnoke@gmail.com>
2024-07-09 19:27:10 +0300

    updated community home view to fetch all forums and regions

--- community/views.py
+++ community/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Region, Forum
 
 # Community home view
 @login_required
 def community_home(request):
-    return render(request, 'community/index.html')
+    regions = Region.objects.all()
+    forums = Forum.objects.all()
+    context = {
+        'regions': regions,
+        'forums': forums
+    }
+
+    return render(request, 'community/index.html', context)
 
 # forum view
 def forum(request):
