8738be228847fe3c853b84a8876f1c667dd52070
nokekip <kipnoke@gmail.com>
2024-07-10 12:36:04 +0300

    implemented websockets to have realtime chat feature

--- community/urls.py
+++ community/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import community_home, forum, create_forum
+from .views import community_home, forum, create_forum, upload_media
 
 
 urlpatterns = [
     path('', community_home, name='community-home'),
     path('forum/<str:forum_id>/', forum, name='forum'),
-    path('create-forum/', create_forum, name='create-forum')
+    path('create-forum/', create_forum, name='create-forum'),
+    path('upload_media/', upload_media, name='upload-media')
 ]
