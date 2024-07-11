b379fef47b169d6b01660c6b2b74ea993fac6d95
nokekip <kipnoke@gmail.com>
2024-07-11 17:39:09 +0300

    defined models for weather alert storage

--- weather_alert/models.py
+++ weather_alert/models.py
@@ -1,3 +1,26 @@
+import uuid
 from django.db import models
+from community.models import Region
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model
+
+# Weather alert model.
+class WeatherAlert(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+# Alerts Media
+class WeatherAlertFile(models.Model):
+    alert = models.ForeignKey(WeatherAlert, related_name='files', on_delete=models.CASCADE)
+    file = models.FileField(upload_to='weather_alerts/')
+
+    def __str___(self):
+        return f'File for alert: {self.alert.title}'
