6ccbc866a364eea39c4f74055beb0d6dcee68770
nokekip <kipnoke@gmail.com>
2024-07-11 18:27:17 +0300

    added class form for adding weather alert

--- weather_alert/forms.py
+++ weather_alert/forms.py
@@ -6,3 +6,13 @@ class AlertSubscriptionForm(forms.ModelForm):
     class Meta:
         model = AlertSubscription
         fields = ['regions']
+
+class WeatherAlertForm(forms.ModelForm):
+    class Meta:
+        model = WeatherAlert
+        fields = ['region', 'title', 'description']
+
+class WeatherAlertFileForm(forms.ModelForm):
+    class Meta:
+        model = WeatherAlertFile
+        fields = ['file']
