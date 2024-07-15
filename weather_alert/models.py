b566add780eca6ae059d667144bd275b3ce8e5a1
nokekip <kipnoke@gmail.com>
2024-07-15 10:26:07 +0300

    refactor modeld for saving media

--- weather_alert/models.py
+++ weather_alert/models.py
@@ -5,25 +5,27 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+# Alerts Media
+class WeatherAlertFile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    file = models.FileField(upload_to='weather_alerts/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str___(self):
+        return f'File for alert: {self.alert.title}'
+
 # Weather alert model.
 class WeatherAlert(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     region = models.ForeignKey(Region, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField()
+    media = models.ForeignKey(WeatherAlertFile, on_delete=models.SET_NULL, blank=True, null=True)
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
-    
-# Alerts Media
-class WeatherAlertFile(models.Model):
-    alert = models.ForeignKey(WeatherAlert, related_name='files', on_delete=models.CASCADE)
-    file = models.FileField(upload_to='weather_alerts/')
-
-    def __str___(self):
-        return f'File for alert: {self.alert.title}'
     
 # ALert subscription
 class AlertSubscription(models.Model):
