ff63b34afbc103b7c7c72c808364aceed33f4e73
nokekip <kipnoke@gmail.com>
2024-07-26 13:55:01 +0300

    registered models to admin

--- community/admin.py
+++ community/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Region, Forum, Media, Message
 
 # Register your models here.
+admin.site.register(Region)
+admin.site.register(Forum)
+admin.site.register(Media)
+admin.site.register(Message)
--- weather_alert/admin.py
+++ weather_alert/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import WeatherAlert, WeatherAlertFile, AlertSubscription
 
 # Register your models here.
+admin.site.register(WeatherAlert)
+admin.site.register(WeatherAlertFile)
+admin.site.register(AlertSubscription)
